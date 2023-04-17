2e5979d0506df7cae57d76e785243bff6de5dc50
hager-dot <hagerelhwarii6633@gmail.com>
2023-04-17 05:46:59 +0200

    finshing what we charge section

--- src/Components/WhatWeCharge.jsx
+++ src/Components/WhatWeCharge.jsx
@@ -3,22 +3,23 @@ import RightArrow from "./RightArrow";
 export default function WhatWeCharge() {
   return (
     <>
-      <div className="mx-auto grid grid-cols-1 lg:grid-cols-2 my-24 md:-translate-x-32 gap-5 xl:gap-0">
-        <div className="md:text-right mt-12 md:mt-16 ml-40 xl:ml-[428px] ">
+    <section>
+      <div className=" grid grid-cols-1 lg:grid-cols-2 my-24 md:-translate-x-32 lg:gap-5 xl:gap-8 px-10">
+        <div className="text-left lg:text-right -mt-12 md:mt-16 md:ml-52 lg:ml-[300px] 2xl:ml-[380px] mb-12 sm:mb-8  ">
           <h3 className="font-thin text-3xl">What we charge</h3>
-          <p className="md:text-3xl xl:text-6xl text-white ">
+          <p className="md:text-4xl xl:text-6xl text-white leading-3 ">
             You get all our knowledge for one simple price
           </p>
         </div>
-        <div className="md:w-[490px] xl:w-[630px] mx-auto bg-zinc-900 rounded-3xl text-center py-12 px-[45px]">
-          <h2 className="text-2xl text-white md:pb-14">
+        <div className="md:w-[490px] xl:w-[630px] bg-zinc-900 rounded-3xl text-center py-12 px-[45px] min-[768px]:max-[990px]:ml-52">
+          <h2 className="text-2xl text-white pb-4 md:pb-14">
             $
             <span className="text-6xl font-bold underline underline-offset-8">
               499
             </span>
             /day
           </h2>
-          <p className="text-3xl font-thin leading-10 mb-6 text-gray-500">
+          <p className="text-xl md:text-[1.8rem] font-thin leading-10 mb-6 text-gray-350">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
@@ -35,6 +36,7 @@ export default function WhatWeCharge() {
           </button>
         </div>
       </div>
+      </section>
     </>
   );
 }
