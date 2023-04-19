0704b424b04b6d13c587cb91b8da85dffc162c51
hager-dot <hagerelhwarii6633@gmail.com>
2023-04-19 05:48:11 +0200

    Numbers increase on scroll

--- src/Components/Number.jsx
+++ src/Components/Number.jsx
@@ -1,43 +1,76 @@
+import { useState, useEffect } from 'react';
+
 export default function Number() {
-    return (
-      <>
-       <div 
-        className="hero h-[950px] relative xl:w-[1390px] mx-auto rounded-3xl  "
+    const [scrollCount, setScrollCount] = useState(0);
+
+    useEffect(() => {
+      function handleScroll() {
+        // Increase the scroll count by 10 when the user scrolls
+        setScrollCount(scrollCount + 10);
+      }
+  
+      window.addEventListener('scroll', handleScroll);
+  
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }, [scrollCount]);
+  return (
+    <>
+      <div
+        className="hero h-[1130px] relative xl:w-[1280px] mx-auto rounded-3xl  "
         style={{
           backgroundImage: `url("https://10wallpaper.com/wallpaper/2560x1600/1610/Ocean_underwater_blu_rays-HD_Retina_Wallpaper_2560x1600.jpg")`,
-        }}>
-       <div
-       className="absolute bg-black  rounded-3xl  px-24   " 
-       >
-         <section className=" lg:mt-0 px-8 md:px-0 pt-12 rounded-3xl">
-        <div className="mx-auto grid grid-cols-1 md:grid-cols-2 gap-20 xl:py-5 ">
-        <div>
-        <div className="card card-compact w-full lg:w-[470px] bg-zinc-900 shadow-xl rounded-3xl mb-16 2xl:-translate-x-10">
-              <figure>
-                <img
-                className="w-full  scale-100 hover:scale-110 cursor-pointer transition ease-in-out duration-700"
-                  src="https://i.imgur.com/eYAKRCB.jpeg"
-                  alt="Shoes"
-                />
-              </figure>
-              <div className="px-10  mx-auto h-80 2xl-h-full">
-                <p className="text-xl py-5 leading-9 px-0 2xl-px-16 text-center  ">
-                "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
-                </p>
-                <div className="card-actions justify-start">
-                 
+        }}
+      >
+        <div className="absolute bg-black rounded-3xl ">
+          <section className=" lg:mt-0 px-8 md:px-0 pt-12 rounded-3xl">
+            <div className="mx-auto grid grid-cols-1 md:grid-cols-2 g xl:py-5 ">
+              <div>
+                <div className="card card-compact w-full lg:w-[470px] bg-zinc-900 shadow-xl rounded-3xl mb-16 2xl:translate-x-20">
+                  <figure>
+                    <img
+                      className="w-full  scale-100 hover:scale-110 cursor-pointer transition ease-in-out duration-700"
+                      src="https://i.imgur.com/eYAKRCB.jpeg"
+                      alt="Shoes"
+                    />
+                  </figure>
+                  <div className="px-10  mx-auto  2xl-h-full">
+                    <p className="text-2xl py-10 leading-9 px-0 2xl-px-16 text-center text-white font-normal">
+                      "Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                      sed diam nonumy eirmod tempor invidunt ut labore et dolore
+                      magna aliquyam erat, sed diam voluptua. At vero eos et
+                      accusam et justo duo dolores et ea rebum. Stet clita kasd
+                      gubergren, no sea takimata sanctus est Lorem ipsum dolor
+                      sit amet."
+                      <hr className="w-[310px] mx-auto my-6"/>
+                      <span className="font-thin">Jane Doe, CEO</span> 
+                    </p>
+                    <div className="card-actions justify-start"></div>
+                  </div>
                 </div>
               </div>
+              <div className=" pl-20 w-[540px] translate-y-24 h-[560px]">
+                <h2 className="text-2xl font-thin mb-2">The numbers</h2>
+                <p className="text-5xl font-bold text-white pb-3">+{`${scrollCount}`}</p>
+                <p className="text-3xl font-thin ">
+                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr.
+                </p>
+                <hr className="w-[350px] my-10 " />
+                <p className="text-5xl font-bold text-white pb-3">{`${scrollCount}`}</p>
+                <p className="text-3xl font-thin">
+                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr.
+                </p>
+                <hr className="w-[350px] my-10 " />
+                <p className="text-5xl font-bold text-white pb-3">{`${scrollCount}`}%</p>
+                <p className="text-3xl font-thin ">
+                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr.
+                </p>
+              </div>
             </div>
+          </section>
         </div>
-        <div>
-            <h2>The numbers</h2>
-            
-        </div>
-        </div>
-      </section>
-       </div>
-       </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
+}
