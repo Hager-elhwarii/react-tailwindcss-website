d32364432361f63aca09cf46f6b2f86608f2e503
hager-dot <hagerelhwarii6633@gmail.com>
2023-04-22 18:47:42 +0200

    Rating component

--- src/Components/WhatWeDont.jsx
+++ src/Components/WhatWeDont.jsx
@@ -6,20 +6,22 @@ export default function WhatWeDont() {
         <div className="container grid grid-cols-1 md:grid-cols-2 gap-12 pr-8 ">
           <div className="lg:translate-x-30 xl:translate-x-44  2xl:translate-x-[17.2rem]">
             <motion.div
-             initial={0}
-             whileInView={{ y:[200,0], opacity: [0.1, 1] }}
-             transition={{ type: "spring", duration: 1}} 
-            className="card card-compact w-full lg:w-[470px] bg-zinc-900 shadow-xl rounded-3xl mb-16">
+              // initial={{ y: 0, opacity: 0 }}
+              whileInView={{ y: [200, 0], opacity: [0.1, 1] }}
+              viewport={{ once: true }}
+              transition={{ type: "spring", duration: 1 }}
+              className="card card-compact w-full lg:w-[470px] bg-zinc-900 shadow-xl rounded-3xl mb-16"
+            >
               <figure>
                 <img
-                className="w-full md:h-72 scale-100 hover:scale-110 cursor-pointer transition ease-in-out duration-700"
+                  className="w-full md:h-72 scale-100 hover:scale-110 cursor-pointer transition ease-in-out duration-700"
                   src="https://themewagon.github.io/klar/img/webp/abstract3.webp"
                   alt="Shoes"
                 />
               </figure>
               <div className="px-10 py-12">
                 <h2 className="text-2xl md:text-4xl text-white">
-                  Ipsum dolor est
+                  Ipsum dolor est test
                 </h2>
                 <p className=" text-xl py-5 ">
                   Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
@@ -34,20 +36,22 @@ export default function WhatWeDont() {
               </div>
             </motion.div>
             <motion.div
-              initial={0}
-              whileInView={{ y:[200,0], opacity: [0.1, 1] }}
-              transition={{ type: "spring", duration: 2}} 
-             className="card card-compact w-full lg:w-[470px] bg-zinc-900 shadow-xl  rounded-3xl">
+              initial={{ y: 0, opacity: 0 }}
+              whileInView={{ y: [200, 0], opacity: [0.1, 1] }}
+              viewport={{ once: true }}
+              transition={{ type: "spring", duration: 2 }}
+              className="card card-compact w-full lg:w-[470px] bg-zinc-900 shadow-xl  rounded-3xl"
+            >
               <figure>
                 <img
-                 className="w-full md:h-72 scale-100 hover:scale-110 cursor-pointer transition ease-in-out duration-700"
+                  className="w-full md:h-72 scale-100 hover:scale-110 cursor-pointer transition ease-in-out duration-700"
                   src="https://themewagon.github.io/klar/img/webp/abstract2.webp"
                   alt="Shoes"
                 />
               </figure>
               <div className="px-10 py-12">
                 <h2 className="text-2xl md:text-4xl text-white">
-                  Ipsum dolor est
+                  Ipsum dolor est test2
                 </h2>
                 <p className=" text-xl py-5 ">
                   Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
@@ -68,13 +72,15 @@ export default function WhatWeDont() {
               There is a lot we don´t know. Here is a small sneak peek
             </p>
             <motion.div
-             initial={0}
-             whileInView={{ y:[200,0], opacity: [0.1, 1] }}
-             transition={{ type: "spring", duration: 1}} 
-             className="card card-compact w-full lg:w-[470px] bg-zinc-900 shadow-xl mt-14 2xl:mt-32  rounded-3xl lg:mb-5">
+              initial={0}
+              whileInView={{ y: [200, 0], opacity: [0.1, 1] }}
+              viewport={{ once: true }}
+              transition={{ type: "spring", duration: 1 }}
+              className="card card-compact w-full lg:w-[470px] bg-zinc-900 shadow-xl mt-14 2xl:mt-32  rounded-3xl lg:mb-5"
+            >
               <figure>
                 <img
-                 className="w-full md:h-72 scale-100 hover:scale-110 cursor-pointer transition ease-in-out duration-700"
+                  className="w-full md:h-72 scale-100 hover:scale-110 cursor-pointer transition ease-in-out duration-700"
                   src="https://themewagon.github.io/klar/img/webp/abstract17.webp"
                   alt="Shoes"
                 />
@@ -97,12 +103,14 @@ export default function WhatWeDont() {
             </motion.div>
             <motion.div
               initial={0}
-              whileInView={{ y:[200,0], opacity: [0.1, 1] }}
-              transition={{ type: "spring", duration: 1}} 
-             className="card card-compact w-full lg:w-[470px] bg-zinc-900 shadow-xl mt-16 rounded-3xl">
+              whileInView={{ y: [200, 0], opacity: [0.1, 1] }}
+              viewport={{ once: true }}
+              transition={{ type: "spring", duration: 1 }}
+              className="card card-compact w-full lg:w-[470px] bg-zinc-900 shadow-xl mt-16 rounded-3xl"
+            >
               <figure>
                 <img
-                 className="w-full md:h-72 scale-100 hover:scale-110 cursor-pointer transition ease-in-out duration-700"
+                  className="w-full md:h-72 scale-100 hover:scale-110 cursor-pointer transition ease-in-out duration-700"
                   src="https://www.psdgraphics.com/file/colorful-triangles-background.jpg"
                   alt="Shoes"
                 />
